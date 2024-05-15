fb11949a78f6a08d0a37cb7615297852ad761411
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-05-15 16:43:37 +0100

    added button

--- barden_book_project/lib/authentication/login.dart
+++ barden_book_project/lib/authentication/login.dart
@@ -1,8 +1,14 @@
+import 'package:barden_book_project/widgets/barden_button.dart';
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
 
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) => Scaffold(
     backgroundColor: Colors.white,
@@ -26,12 +32,23 @@ class Login extends StatelessWidget {
     ),
   );
 
-    Widget _buildRightLoginBox(BuildContext context) => Container(
+  Widget _buildRightLoginBox(BuildContext context) => Container(
     width: 400,
     height: MediaQuery.sizeOf(context).height * 0.7,
     decoration: const BoxDecoration(    
       color: Color.fromARGB(255, 224, 224, 224),
       borderRadius: BorderRadius.only(topRight: Radius.circular(4), bottomRight: Radius.circular(4))
+    ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        BardenButton(
+          text: "Login", 
+          onPressed: () {
+            // authenticate here
+          }
+        )
+      ],
     ),
   );
 }
---
+++ barden_book_project/lib/widgets/barden_button.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class BardenButton extends StatefulWidget {
+  const BardenButton({super.key, required this.text, required this.onPressed});
+
+  final String text;
+  final VoidCallback onPressed;
+
+  @override
+  State<BardenButton> createState() => _BardenButtonState();
+}
+
+class _BardenButtonState extends State<BardenButton> {
+  bool isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        onEnter: (_) => setState(() => isHovered = true),
+        onExit: (_) => setState(() => isHovered = false),
+        child: GestureDetector(
+          onTap: () => widget.onPressed(),
+          child: AnimatedOpacity(
+            opacity: isHovered ? 0.7 : 1.0,
+            duration: const Duration(milliseconds: 200),
+            child: Container(
+              width: 200,
+              height: 40,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: const Color.fromARGB(255, 100, 189, 33)                   
+              ),
+              child: Center(
+                child: Text(
+                  widget.text,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 13,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
