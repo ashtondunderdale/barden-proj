a6930c72d6391ea84bf020e97be597bef2daa5b8
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-05-17 10:55:16 +0100

    set up blob http call

--- barden_book_project/lib/home/services/blob.dart
+++ barden_book_project/lib/home/services/blob.dart
@@ -1,15 +1,56 @@
 import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:http/http.dart' as http;
 
 class BlobService {
+  final String storageAccountName = '';
+  final String containerName = '';
+  final String sasToken = '';
 
   void getFiles() async {
     try {
-        var chosenFiles = await FilePickerWeb.platform.pickFiles();
+      var chosenFiles = await FilePicker.platform.pickFiles(allowMultiple: true);    
 
-      
+      if (chosenFiles == null || chosenFiles.files.isEmpty) {
+        return;
+      }
+
+      for (var file in chosenFiles.files) {
+        await uploadFileToAzure(file);
+      }
 
     } catch (exception) {
       print(exception);
+    }
+  }
+
+  Future<bool> uploadFileToAzure(PlatformFile file) async {
+    try {
+      final fileName = file.name;
+      final fileBytes = file.bytes;
+
+      print("Uploading File: $fileName");
+
+      if (fileBytes == null) {
+        throw Exception("File bytes are null");
+      }
+
+      final url = 'https://azureBlobUrl';
+
+      final response = await http.put(
+        Uri.parse(url),
+        headers: {
+          'x-ms-blob-type': 'BlockBlob',
+          'Content-Type': file.extension ?? 'application/octet-stream',
+        },
+        body: fileBytes,
+      );
+
+      return response.statusCode == 200;
+      
+    } catch (exception) {
+      print('Error uploading file: $exception');
+      return false;
     }
   }
 }
